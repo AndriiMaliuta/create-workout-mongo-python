25dd26a4a83859e6fc037adb7222b2707f77aa7f
malandr <quadr988@gmail.com>
2022-10-05 19:54:06 +0300

    init

--- main.py
+++ main.py
@@ -13,22 +13,27 @@ def create_workout(request):
         client = pymongo.MongoClient(os.getenv("DB_URL"))
         db = client['workouts']
         work_collection = db['workouts']
+        today = datetime.datetime.now()
         if "comments" in request_json \
+                and "sets" in request_json \
                 and "workout_date" in request_json \
-                and "workout_type" in request_json \
-                and "creation_date" in request_json:
+                and "workout_type" in request_json:
             workout = {
-                "record": datetime.datetime.utcnow().timestamp(),
-                "sets": 0,
+                "record": round(datetime.datetime.now().timestamp()),
+                "sets": request_json["sets"],
                 "comments": request_json["comments"],
                 "workout_date": request_json["workout_date"],
                 "workout_type": request_json["workout_type"],
-                "creation_date": request_json["creation_date"],
-                "day": request_json["day"],
-                "week": request_json["week"],
-                "user_id": 1,
-                "month": request_json["month"]
+                "creation_date": today,
+                "day": today.strftime("%A"),
+                "week": week_number_of_month(today),
+                "user_id": request_json["user_id"],
+                "month": today.strftime("%B")
             }
             id = work_collection.insert_one(workout).inserted_id
             return 'created workout {}!'.format(id)
     return "TEST"
+
+
+def week_number_of_month(date_value):
+    return (date_value.isocalendar()[1] - date_value.replace(day=1).isocalendar()[1] + 1)
