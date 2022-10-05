267f4b3b48bad50a7b43aa8e0ecb219a31e2a212
malandr <quadr988@gmail.com>
2022-10-05 16:49:32 +0300

    init

--- main.py
+++ main.py
@@ -7,22 +7,28 @@ import datetime
 
 @functions_framework.http
 def create_workout(request):
-    request_json = request.get_json(silent=True)
-    # request_args = request.args
-    client = pymongo.MongoClient(os.getenv("DB_URL"))
-    db = client['workouts']
-    work_collection = db['workouts']
-    workout = {
-        "record": datetime.datetime.utcnow().timestamp(),
-        "sets": 0,
-        "comments": request_json["comments"],
-        "workout_date": request_json["workout_date"],
-        "workout_type": request_json["workout_type"],
-        "creation_date": request_json["creation_date"],
-        "day": request_json["day"],
-        "week": request_json["week"],
-        "user_id": 1,
-        "month": request_json["month"]
-    }
-    id = work_collection.insert_one(workout).inserted_id
-    return 'created workout {}!'.format(id)
+    if request.method == "POST":
+        request_json = request.get_json(silent=True)
+        # request_args = request.args
+        client = pymongo.MongoClient(os.getenv("DB_URL"))
+        db = client['workouts']
+        work_collection = db['workouts']
+        if "comments" in request_json \
+                and "workout_date" in request_json \
+                and "workout_type" in request_json \
+                and "creation_date" in request_json:
+            workout = {
+                "record": datetime.datetime.utcnow().timestamp(),
+                "sets": 0,
+                "comments": request_json["comments"],
+                "workout_date": request_json["workout_date"],
+                "workout_type": request_json["workout_type"],
+                "creation_date": request_json["creation_date"],
+                "day": request_json["day"],
+                "week": request_json["week"],
+                "user_id": 1,
+                "month": request_json["month"]
+            }
+            id = work_collection.insert_one(workout).inserted_id
+            return 'created workout {}!'.format(id)
+    return "TEST"
